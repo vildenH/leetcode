623133b632a74768a274818a12a7a2744deec4bc
wh <vildenhh@gmail.com>
2017-07-01 12:39:38 +0800

    nice meage

--- 21. Merge Two Sorted Lists/Soluation.java
+++ 21. Merge Two Sorted Lists/Soluation.java
@@ -9,7 +9,7 @@
 public class Solution {
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-		ListNode fake=new ListNode();
+		ListNode fake=new ListNode(0);
 		ListNode now=fake;
 		while (l1 != null && l2 != null) {
 			if (l1.val < l2.val) {

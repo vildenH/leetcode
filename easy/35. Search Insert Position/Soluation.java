6173c1ac791c23299b493c9be61de3e0be301e6f
wh <vildenhh@gmail.com>
2017-07-02 11:32:59 +0800

    binary search

---
+++ easy/35. Search Insert Position/Soluation.java
@@ -0,0 +1,14 @@
+
+//找到排序数组中 插入target的位置。 二分法。
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}

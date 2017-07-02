c21c2ad28783fc4f312b2cefdd4e20b6327b8531
wh <vildenhh@gmail.com>
2017-07-02 12:42:42 +0800

    Array

---
+++ easy/624. Maximum Distance in Arrays/Soluation.java
@@ -0,0 +1,22 @@
+//要从不同数组中取元素，所以可以不断比较。
+
+
+public class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int min=arrays.get(0).get(0);
+        int max=arrays.get(0).get(arrays.get(0).size()-1);
+        int result=-1000000;
+        for(int i=1;i<arrays.size();i++)
+        {
+            result=Math.max(result,Math.abs(min-arrays.get(i).get(arrays.get(i).size()-1)));
+            result=Math.max(result,Math.abs(max-arrays.get(i).get(0)));
+            
+
+
+            min=Math.min(min,arrays.get(i).get(0));//取第一个
+            max=Math.max(max,arrays.get(i).get(arrays.get(i).size()-1));//取第最后一个不断比较大小
+        }
+
+        return result;
+    }
+}

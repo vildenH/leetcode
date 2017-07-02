ec3073d8969d012e5212c4512e2b5aec3b775a5f
wh <vildenhh@gmail.com>
2017-07-02 10:25:33 +0800

    Remove Element

--- easy/27. Remove Element/Soluation.java
+++ easy/27. Remove Element/Soluation.java
@@ -1,12 +1,20 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
         int ans=0;
+        int slow=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==val)
             {
                 ans=ans+1;
+                
+            }
+            else
+            {
+                nums[slow]=nums[i];
+                slow++;
             }
         }
+        return ans;
     }
 }
